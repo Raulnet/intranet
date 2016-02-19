26aa91b325283c687f4bf74a464c6398a0e28537
laurent <hormm@hotmail.fr>
2016-02-19 23:02:18 +0100

    up contact club by team

--- src/Ffjv/FoBundle/Controller/TeamsController.php
+++ src/Ffjv/FoBundle/Controller/TeamsController.php
@@ -6,6 +6,7 @@ use Ffjv\BoBundle\Entity\Teams;
 use Ffjv\BoBundle\Form\TeamsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Ffjv\BoBundle\Entity\Messages;
 
 class TeamsController extends Controller
 {
@@ -19,10 +20,15 @@ class TeamsController extends Controller
         $team = $em->getRepository('FfjvBoBundle:Teams')->find($teamId);
         $editForm = $this->getFormTeamEdit($team);
         $deleteForm = $this->getDeleteForm($team);
+        $contactForm = $this->getContactForm($this->generateUrl('fo_teams_clubs_contact', ['teamId' => $teamId]), array(
+            'user' => $this->getUser()->getId(),
+            'club' => $team->getClub()->getId()
+        ));
         return $this->render('FfjvFoBundle:Teams:show.html.twig', [
             'team' => $team,
             'editForm' => $editForm->createView(),
-            'deleteForm' => $deleteForm->createView()
+            'deleteForm' => $deleteForm->createView(),
+            'contact_form' => $contactForm->createView()
         ]);
     }
 
@@ -39,7 +45,7 @@ class TeamsController extends Controller
             return $this->redirectToRoute('fo_profile_show', array('userUsername' => $this->getUser()->getUsername()));
         }
         if($team->getClub()->getUser() != $this->getUser()){
-            $this->addFlash('error', 'une erreur c\est produite . Vous n\'êtes pas autorisez à modifier cette equipe .' );
+            $this->addFlash('error', 'une erreur c\'est produite . Vous n\'êtes pas autorisez à modifier cette equipe .' );
             return $this->redirectToRoute('fo_profile_show', array('userUsername' => $this->getUser()->getUsername()));
         }
         $editForm = $this->getFormTeamEdit($team);
@@ -50,7 +56,7 @@ class TeamsController extends Controller
             $em->flush();
             $this->addFlash('success', 'Votre Team a été mise a jours !');
         } else {
-            $this->addFlash('error', 'une erreur c\est produite . Votre team n\'a pus être mise à jour.');
+            $this->addFlash('error', 'une erreur c\'est produite . Votre team n\'a pus être mise à jour.');
         }
         return $this->render('FfjvFoBundle:Teams:show.html.twig', [
             'team'          => $team,
@@ -88,6 +94,54 @@ class TeamsController extends Controller
     }
 
     /**
+     * @param Request $request
+     * @param int $teamId
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function contactClubTeamAction(Request $request, $teamId = 0){
+
+        $em = $this->getDoctrine()->getManager();
+        $team = $em->getRepository('FfjvBoBundle:Teams')->find($teamId);
+        if(!$team){
+            $this->addFlash('error', 'Une erreur c\'est produite');
+            return $this->redirectToRoute('fo_profile_show', array('id' => $this->getUser()->getId()));
+        }
+
+        $form = $this->getContactForm();
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+            $data = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $id = $data['club'];
+            $club = $em->getRepository('FfjvBoBundle:Clubs')->find($id);
+            if (!$club) {
+                throw $this->createNotFoundException('Unable to find Clubs club.');
+            }
+            // create and save Mesage
+            $message = new Messages();
+            $message->setAuthorUser($this->getUser());
+            $message->setMessage($data['content']);
+            $message->setSubject($data['subject']);
+            $message->setClub($club);
+            $message->setEmail($club->getEmail());
+            $em->persist($message);
+            $em->flush();
+            //send message
+            if($this->get('contact')->contactClub($message)){
+                $this->addFlash('success', 'Votre message a bien été envoyer');
+            } else {
+                $this->addFlash('error', 'Une erreur c\'est produite ! Votre message n\'a pus être envoyé .');
+            }
+            return $this->redirectToRoute('fo_teams_show', array('teamId' => $team->getId()));
+        }
+        $this->addFlash('error', 'Une erreur c\'est produite');
+        return $this->redirectToRoute('fo_profile_show', array('id' => $this->getUser()->getId()));
+    }
+
+    /** ********* PRIVATE ********** */
+
+    /**
      * @param Teams $team
      * @return $this|\Symfony\Component\Form\FormInterface
      */
@@ -118,5 +172,18 @@ class TeamsController extends Controller
             ])
             ->getForm();
 
+    }
+
+    /**
+     * @param string $url
+     * @param array $data
+     * @return \Symfony\Component\Form\Form|\Symfony\Component\Form\FormInterface
+     */
+    private function getContactForm($url = '', $data = array()){
+        $form = $this->get('contact')->getFormContactClub($url, $data);
+        $form->add('submit', 'submit', array(
+            'label' => 'envoyer'
+        ));
+        return $form;
     }
 }
